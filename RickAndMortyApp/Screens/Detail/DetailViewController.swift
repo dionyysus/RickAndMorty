29f084d08029eee34948a65f7bf97c284bb5f81d
dionyysus <gizemcsk1@gmail.com>
2023-08-29 15:50:22 +0300

    search bar added

--- RickAndMortyApp/Screens/Detail/DetailViewController.swift
+++ RickAndMortyApp/Screens/Detail/DetailViewController.swift
@@ -9,15 +9,9 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        detailView.layer.borderWidth = 1
-        detailView.layer.borderColor = UIColor.lightGray.cgColor
-        detailView.layer.shadowOpacity = 1
-        detailView.layer.shadowOffset = .zero
-        detailView.layer.shadowRadius = 1
     }
     
 }
