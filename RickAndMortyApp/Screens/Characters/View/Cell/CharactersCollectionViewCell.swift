e12c73960dce03815c7e0ca894f6399d5c355f8e
dionyysus <gizemcsk1@gmail.com>
2023-08-29 03:00:58 +0300

    characters collection view cell created and design completed

---
+++ RickAndMortyApp/Screens/Characters/View/Cell/CharactersCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersCollectionViewCell.swift
+//  RickAndMortyApp
+//
+//  Created by Gizem Coşkun on 29.08.2023.
+//
+
+import UIKit
+
+class CharactersCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
