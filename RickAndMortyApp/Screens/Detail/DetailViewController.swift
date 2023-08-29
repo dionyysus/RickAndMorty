06462578b0d4571c6267fe4b04828766c27c24b6
dionyysus <gizemcsk1@gmail.com>
2023-08-29 15:39:10 +0300

    detail storyboard updated

--- RickAndMortyApp/Screens/Detail/DetailViewController.swift
+++ RickAndMortyApp/Screens/Detail/DetailViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var detailView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        detailView.layer.borderWidth = 1
+        detailView.layer.borderColor = UIColor.lightGray.cgColor
+        detailView.layer.shadowOpacity = 1
+        detailView.layer.shadowOffset = .zero
+        detailView.layer.shadowRadius = 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
