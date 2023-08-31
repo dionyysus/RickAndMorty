91e09f7eb4e81db6d71289d41ada6c709ceaf06f
dionyysus <gizemcsk1@gmail.com>
2023-08-31 22:45:43 +0300

    programmatic changes

--- RickAndMortyApp/Screens/Home/HomeViewController.swift
+++ RickAndMortyApp/Screens/Home/HomeViewController.swift
@@ -8,26 +8,38 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
-    @IBOutlet weak var charactersButton: UIButton!
-    @IBOutlet weak var locationsButton: UIButton!
-    @IBOutlet weak var episodesButton: UIButton!
+    
+    private let charactersButton: UIButton = {
+        let charactersButton = UIButton()
+        charactersButton.backgroundColor = .systemBlue
+        charactersButton.setTitle("My Button", for: .normal)
+        return charactersButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-   
-    @IBAction func characterButtonTapped() {
-        let story = UIStoryboard(name: "Characters", bundle: nil)
-        let charactersController = story.instantiateViewController(withIdentifier: "CharactersViewController") as! CharactersViewController
-        navigationController?.pushViewController(charactersController, animated: true)
-    }
-    
-    //TODO: will create
-    @IBAction func locationButtonTapped() {
-    }
-    //TODO: will create
-    @IBAction func episodeButtonTapped() {
+
+        self.view.addSubview(charactersButton)
+        charactersButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            charactersButton.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
+            charactersButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            charactersButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            charactersButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            
+        ])
+        
+        if let backgroundImage = UIImage(named: "home.png") {
+            let background = UIView()
+            background.translatesAutoresizingMaskIntoConstraints = false
+            background.backgroundColor = UIColor(patternImage: backgroundImage)
+            view.addSubview(background)
+            
+            let imageSize = backgroundImage.size
+            background.frame = CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height)
+            
+            background.center = view.center
+        }
     }
 }
-
