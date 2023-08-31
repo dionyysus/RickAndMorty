b5595dae941511caf441ae3e88d3ff7cfdc5180f
dionyysus <gizemcsk1@gmail.com>
2023-09-01 01:12:59 +0300

    character view cell constraits added

--- RickAndMortyApp/Screens/Characters/View/Cell/CharactersCollectionViewCell.swift
+++ RickAndMortyApp/Screens/Characters/View/Cell/CharactersCollectionViewCell.swift
@@ -8,14 +8,8 @@
 import UIKit
 
 class CharactersCollectionViewCell: UICollectionViewCell {
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.distribution = .equalSpacing
-        stackView.backgroundColor = .clear
-        stackView.spacing = 10
-        return stackView
-    }()
+    
+    static let identifier = "CharactersCollectionViewCell"
     
     let characterImageView: UIImageView = {
         let characterImageView = UIImageView()
@@ -30,9 +24,26 @@ class CharactersCollectionViewCell: UICollectionViewCell {
         return nameLabel
     }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        contentView.addSubview(characterImageView)
+        contentView.addSubview(nameLabel)
         
+        NSLayoutConstraint.activate([
+            characterImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            characterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            characterImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 8),
+            characterImageView.widthAnchor.constraint(equalToConstant: 80),
+            characterImageView.heightAnchor.constraint(equalToConstant: 80),
+            
+            nameLabel.topAnchor.constraint(equalTo: characterImageView.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 8),
+        ])
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("erroor")
+    }
+        
 }
