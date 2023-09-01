195e76190cf181562f441dafde18579a1705ee2b
dionyysus <gizemcsk1@gmail.com>
2023-09-01 21:49:29 +0300

    navigation item title changed (finallyyyy after 3-4 hours jkdshf)

--- RickAndMortyApp/Screens/Characters/CharactersViewController.swift
+++ RickAndMortyApp/Screens/Characters/CharactersViewController.swift
@@ -9,17 +9,17 @@ import UIKit
 
 final class CharactersViewController: UIViewController {
     
-    private lazy var titleLabel: UILabel = {
-        let titleLabel = UILabel()
-        titleLabel.text = "Characters"
-        titleLabel.textColor = .black
-        titleLabel.textAlignment = .center
-        titleLabel.backgroundColor = .systemPink
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        titleLabel.numberOfLines = 0
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        return titleLabel
-    }()
+//    private lazy var titleLabel: UILabel = {
+//        let titleLabel = UILabel()
+//        titleLabel.text = "Characters"
+//        titleLabel.textColor = .black
+//        titleLabel.textAlignment = .center
+//        titleLabel.backgroundColor = .systemPink
+//        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+//        titleLabel.numberOfLines = 0
+//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+//        return titleLabel
+//    }()
     
     private let charactersCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -28,11 +28,11 @@ final class CharactersViewController: UIViewController {
         charactersCollectionView.translatesAutoresizingMaskIntoConstraints = false
         return charactersCollectionView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(titleLabel)
+        //view.addSubview(titleLabel)
         view.addSubview(charactersCollectionView)
         
         setupConstraints()
@@ -40,23 +40,20 @@ final class CharactersViewController: UIViewController {
         charactersCollectionView.register(CharactersCollectionViewCell.self, forCellWithReuseIdentifier: CharactersCollectionViewCell.identifier)
         charactersCollectionView.delegate = self
         charactersCollectionView.dataSource = self
+        
+        self.navigationItem.title = "Characters"
     }
     
     func setupConstraints() {
-           
-           NSLayoutConstraint.activate([
+        
+        NSLayoutConstraint.activate([
 
-               titleLabel.topAnchor.constraint(equalTo: view.topAnchor),
-               titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-               titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-               
-               charactersCollectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-               charactersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-               charactersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-               charactersCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-           ])
-       }
-   
+            charactersCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            charactersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            charactersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            charactersCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
 }
 
 //MARK: Collection View Data Source
@@ -64,17 +61,17 @@ extension CharactersViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharactersCollectionViewCell.identifier, for: indexPath) as? CharactersCollectionViewCell else {
             return UICollectionViewCell()
         }
         return cell
     }
-
+    
     //TODO: update it with programmatic ui
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-
+        
         let storyBoard = UIStoryboard(name: "Detail", bundle: nil)
         let gotoDetailController = storyBoard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
         navigationController?.pushViewController(gotoDetailController, animated: true)
@@ -84,25 +81,25 @@ extension CharactersViewController: UICollectionViewDataSource {
 
 //MARK: Collection View Delegate
 extension CharactersViewController: UICollectionViewDelegate {
-
+    
 }
 
 extension CharactersViewController: UICollectionViewDelegateFlowLayout{
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-            return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-        }
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width/2 - 20, height: 250)
-       }
+    }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-            return 0
-        }
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-            return 0
-        }
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
 
 
