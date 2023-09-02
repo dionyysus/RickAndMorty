1b7e90a417845c1dac99f7bd91b6da752827e9e5
dionyysus <gizemcsk1@gmail.com>
2023-09-02 15:46:44 +0300

    characterResponse created and character view model updated

--- RickAndMortyApp/Screens/Characters/ViewModel/CharacterViewModel.swift
+++ RickAndMortyApp/Screens/Characters/ViewModel/CharacterViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class CharacterViewModel {
+    
+    var characters: [Characters] = [] //all characters
+    
+    private var apiManager: APIManager?
+    
+    init(apiManager: APIManager) {
+        self.apiManager = apiManager
+    }
+    
+    func fetchMovies(completion: @escaping () -> Void) {
+        APIManager.shared.execute(url: APIManager.shared.baseURL) { (data: CharacterResponse?) in
+            self.characters = data?.results ?? []
+            DispatchQueue.main.async {
+                completion()
+            }
+        }
+    }
+}
