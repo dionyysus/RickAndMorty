0c6fa459aa8893aa37edac130d4ce079854c27ad
dionyysus <gizemcsk1@gmail.com>
2023-09-02 15:30:09 +0300

    api manager api url added as generic

--- RickAndMortyApp/Services/APIManager.swift
+++ RickAndMortyApp/Services/APIManager.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+// rick and morty api: https://rickandmortyapi.com/api
+//character endpoint: /character
+//locations endpoint: /location
+//episodes endpoint: /episode
+
+class APIManager {
+    
+    static let shared = APIManager()
+    
+//    private var favoriteMoviesArray: [Movie] = []
+//    private var categoryMoviesArray: [Genre] = []
+//    private var searchedMoviesArray: [Movie] = []
+    
+    var baseURL = "https://rickandmortyapi.com/api/"
+    var characterEndPoint = "/character"
+
+    private init() { }
+   
+    func execute<T: Decodable>(url: String, completion: @escaping(T?) -> ()) {
+        guard let url = URL(string: url) else { return }
+        
+        let session = URLSession(configuration: .default)
+        
+        let dataTask = session.dataTask(with: url) { (data, response, error) in
+            if error == nil {
+                if let data = data {
+                    do {
+                        let decodedData = try JSONDecoder().decode(T.self, from: data)
+                        completion(decodedData)
+                    } catch {
+                        print("parsing error \(error)")
+                    }
+                } else {
+                    print("failed fetch data")
+                    completion(nil)
+                }
+            } else {
+                print("error data task \(String(describing: error))")
+                completion(nil)
+            }
+        }
+        dataTask.resume()
+    }
+}
+
