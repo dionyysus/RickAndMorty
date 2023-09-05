42d0d0a498ba55ffbf606db260a5ea8bd94a3c20
dionyysus <gizemcsk1@gmail.com>
2023-09-06 02:17:30 +0300

    update

--- RickAndMortyApp/Screens/Home/HomeViewController.swift
+++ RickAndMortyApp/Screens/Home/HomeViewController.swift
@@ -9,6 +9,53 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    let homeImageView: UIImageView = {
+        let homeImageView = UIImageView()
+        homeImageView.image = UIImage(named: "background.png")
+        homeImageView.translatesAutoresizingMaskIntoConstraints = false
+        homeImageView.contentMode = .scaleAspectFill
+        homeImageView.layer.cornerRadius = 10.0
+        homeImageView.layer.masksToBounds = true
+        return homeImageView
+    }()
+    
+    private let charactersCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let charactersCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        charactersCollectionView.backgroundColor = .clear
+        charactersCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        charactersCollectionView.showsHorizontalScrollIndicator = false
+        return charactersCollectionView
+    }()
+    
+    private let episodeCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let episodeCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        episodeCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        episodeCollectionView.backgroundColor = .clear
+        return episodeCollectionView
+    }()
+    
+    var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "Characters"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        titleLabel.textColor = .white
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        return titleLabel
+    }()
+    
+    var episodeTitleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "Episodes"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        titleLabel.textColor = .white
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        return titleLabel
+    }()
+    
     private lazy var charactersButton: UIButton = {
         let charactersButton = UIButton()
         charactersButton.backgroundColor = UIColor(red: 46/255, green: 56/255, blue: 61/255, alpha: 0.7)
@@ -57,17 +104,56 @@ class HomeViewController: UIViewController {
         backgroundImage.contentMode = .scaleAspectFill
         view.addSubview(backgroundImage)
         view.addSubview(stackView)
+        view.addSubview(homeImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(charactersCollectionView)
+        view.addSubview(episodeTitleLabel)
+        view.addSubview(episodeCollectionView)
+        
+        charactersCollectionView.register(CharactersCollectionViewCell.self, forCellWithReuseIdentifier: CharactersCollectionViewCell.identifier)
+        charactersCollectionView.delegate = self
+        charactersCollectionView.dataSource = self
+        
+        episodeCollectionView.register(EpisodeCollectionViewCell.self, forCellWithReuseIdentifier: EpisodeCollectionViewCell.identifier)
+        episodeCollectionView.delegate = self
+        episodeCollectionView.dataSource = self
         
         backgroundImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
+            homeImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            homeImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            homeImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            homeImageView.heightAnchor.constraint(equalToConstant: 300),
+            
+            titleLabel.topAnchor.constraint(equalTo: homeImageView.bottomAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            
+            charactersCollectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            charactersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            charactersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            charactersCollectionView.heightAnchor.constraint(equalToConstant: 200),
+            
+            episodeTitleLabel.topAnchor.constraint(equalTo: charactersCollectionView.bottomAnchor, constant: 10),
+            episodeTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            
+            episodeCollectionView.topAnchor.constraint(equalTo: episodeTitleLabel.bottomAnchor),
+            episodeCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            episodeCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            episodeCollectionView.heightAnchor.constraint(equalToConstant: 400),
+            
+            
+            stackView.topAnchor.constraint(equalTo: episodeCollectionView.bottomAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+
             backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
         ])
         
         charactersButton.addTarget(self, action: #selector(didTapCharacterButton), for: .touchUpInside)
@@ -105,5 +191,56 @@ class HomeViewController: UIViewController {
                 button.heightAnchor.constraint(equalToConstant: 50)
             ])
         }
+    }
+}
+//MARK: Collection View Data Source
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == charactersCollectionView {
+            return 5
+        } else {
+            return 10
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == charactersCollectionView {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharactersCollectionViewCell.identifier, for: indexPath) as? CharactersCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.nameLabel.textColor = .white
+            return cell
+        } else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EpisodeCollectionViewCell.identifier, for: indexPath) as? EpisodeCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            cell.nameLabel.textColor = .white
+            return cell
+        }
+        
+    }
+   
+}
+
+//MARK: Collection View Delegate
+extension HomeViewController: UICollectionViewDelegate {
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5.0, left: 15.0, bottom: 5.0, right: 15.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width/2 - 20, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
     }
 }
