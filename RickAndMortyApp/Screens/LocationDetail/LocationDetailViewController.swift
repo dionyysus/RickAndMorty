ad5b72ff7ee3880e7b98975ac14b58ae9149f4a2
dionyysus <gizemcsk1@gmail.com>
2023-09-06 00:53:49 +0300

    location detail view controller design updated

--- RickAndMortyApp/Screens/LocationDetail/LocationDetailViewController.swift
+++ RickAndMortyApp/Screens/LocationDetail/LocationDetailViewController.swift
@@ -8,31 +8,48 @@
 import UIKit
 
 class LocationDetailViewController: UIViewController {
-        
-    private lazy var detailImageView: UIImageView = {
-        let detailImageView = UIImageView()
-        detailImageView.image = UIImage(named: "LaunchPhoto.png")
-        detailImageView.translatesAutoresizingMaskIntoConstraints = false
-        detailImageView.contentMode = .scaleAspectFill
-        detailImageView.layer.cornerRadius = 15.0
-        detailImageView.layer.masksToBounds = true
-        return detailImageView
+    
+    private lazy var locationDetailImageView: UIImageView = {
+        let locationDetailImageView = UIImageView()
+        locationDetailImageView.image = UIImage(named: "LaunchPhoto.png")
+        locationDetailImageView.translatesAutoresizingMaskIntoConstraints = false
+        locationDetailImageView.contentMode = .scaleAspectFill
+        locationDetailImageView.layer.cornerRadius = 15.0
+        locationDetailImageView.layer.masksToBounds = true
+        return locationDetailImageView
     }()
+    
+    private let charactersCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let charactersCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        charactersCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        return charactersCollectionView
+    }()
+    
     
     private lazy var titleLabel: UILabel = {
         let titleLabel = UILabel()
-        titleLabel.text = "Rick Sanchez"
-        titleLabel.textColor = .black
-        titleLabel.textAlignment = .center
+        titleLabel.text = "Earth"
+        titleLabel.textColor = .blue
         titleLabel.font = UIFont.boldSystemFont(ofSize: 25.0)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         return titleLabel
     }()
     
+    private lazy var residentsLabel: UILabel = {
+        let residentsLabel = UILabel()
+        residentsLabel.text = "Residents"
+        residentsLabel.textColor = .black
+        residentsLabel.font = UIFont.boldSystemFont(ofSize: 25.0)
+        residentsLabel.translatesAutoresizingMaskIntoConstraints = false
+        return residentsLabel
+    }()
+    
     private lazy var detailView: UIView = {
         let detailView = UIView()
         detailView.layer.cornerRadius = 20.0
-        detailView.layer.borderColor = UIColor.lightGray.cgColor
+        detailView.layer.borderColor = UIColor.systemMint.cgColor
         detailView.layer.borderWidth = 2.0
         detailView.backgroundColor = .white
         detailView.translatesAutoresizingMaskIntoConstraints = false
@@ -45,118 +62,80 @@ class LocationDetailViewController: UIViewController {
         stackView.backgroundColor = .clear
         stackView.axis = .vertical
         stackView.alignment = .leading
-        stackView.spacing = 3
+        stackView.spacing = 13
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private lazy var speciesStackView: UIStackView = {
+    private lazy var titleAndDetailViewtackView: UIStackView = {
         let stackView = UIStackView()
         stackView.distribution = .equalSpacing
         stackView.backgroundColor = .clear
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.spacing = 3
+        stackView.axis = .vertical
+        stackView.spacing = 15
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private lazy var genderStackView: UIStackView = {
+    private lazy var titleStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.distribution = .equalSpacing
+        stackView.distribution = .fillProportionally
         stackView.backgroundColor = .clear
         stackView.axis = .horizontal
         stackView.alignment = .center
-        stackView.spacing = 3
+        stackView.spacing = 5
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private lazy var origineStackView: UIStackView = {
+    private lazy var firstEpisodeStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.distribution = .equalSpacing
+        stackView.distribution = .fillProportionally
         stackView.backgroundColor = .clear
         stackView.axis = .horizontal
         stackView.alignment = .center
-        stackView.spacing = 3
+        stackView.spacing = 5
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private lazy var locationStackView: UIStackView = {
+    private lazy var dimensionStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.distribution = .equalSpacing
+        stackView.distribution = .fillProportionally
         stackView.backgroundColor = .clear
         stackView.axis = .horizontal
         stackView.alignment = .center
-        stackView.spacing = 3
+        stackView.spacing = 5
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private lazy var stackViewImageAndLabel: UIStackView = {
-        let stackViewImageAndLabel = UIStackView()
-        stackViewImageAndLabel.distribution = .equalSpacing
-        stackViewImageAndLabel.backgroundColor = .clear
-        stackViewImageAndLabel.axis = .horizontal
-        stackViewImageAndLabel.alignment = .center
-        stackViewImageAndLabel.spacing = 3
-        stackViewImageAndLabel.translatesAutoresizingMaskIntoConstraints = false
-        return stackViewImageAndLabel
+    
+    private lazy var typeLabel: UILabel = {
+        let typeLabel = UILabel()
+        typeLabel.text = "Type: "
+        typeLabel.textColor = .orange
+        typeLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        return typeLabel
     }()
     
-    private let statusImageView: UIImageView = {
-        let statusImageView = UIImageView()
-        statusImageView.image = UIImage(named: "greenCircle.png")
-        statusImageView.widthAnchor.constraint(equalToConstant: 25).isActive = true
-        statusImageView.heightAnchor.constraint(equalToConstant: 25).isActive = true
-        statusImageView.translatesAutoresizingMaskIntoConstraints = false
-        return statusImageView
+    private lazy var firstEpisodeLabel: UILabel = {
+        let firstEpisodeLabel = UILabel()
+        firstEpisodeLabel.text = "First Episode: "
+        firstEpisodeLabel.textColor = .orange
+        firstEpisodeLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        firstEpisodeLabel.translatesAutoresizingMaskIntoConstraints = false
+        return firstEpisodeLabel
     }()
     
-    private lazy var statusLabel: UILabel = {
-        let statusLabel = UILabel()
-        statusLabel.text = "Staus: "
-        statusLabel.textColor = .orange
-        statusLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
-        return statusLabel
-    }()
-    
-    private lazy var spesiecLabel: UILabel = {
-        let spesiecLabel = UILabel()
-        spesiecLabel.text = "Species: "
-        spesiecLabel.textColor = .orange
-        spesiecLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        spesiecLabel.translatesAutoresizingMaskIntoConstraints = false
-        return spesiecLabel
-    }()
-    
-    private lazy var genderLabel: UILabel = {
-        let spesiecLabel = UILabel()
-        spesiecLabel.text = "Gender: "
-        spesiecLabel.textColor = .orange
-        spesiecLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        spesiecLabel.translatesAutoresizingMaskIntoConstraints = false
-        return spesiecLabel
-    }()
-    
-    private lazy var origineLabel: UILabel = {
-        let spesiecLabel = UILabel()
-        spesiecLabel.text = "Origine: "
-        spesiecLabel.textColor = .orange
-        spesiecLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        spesiecLabel.translatesAutoresizingMaskIntoConstraints = false
-        return spesiecLabel
-    }()
-    
-    private lazy var locationLabel: UILabel = {
-        let spesiecLabel = UILabel()
-        spesiecLabel.text = "Location: "
-        spesiecLabel.textColor = .orange
-        spesiecLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        spesiecLabel.translatesAutoresizingMaskIntoConstraints = false
-        return spesiecLabel
+    private lazy var dimensionLabel: UILabel = {
+        let dimensionLabel = UILabel()
+        dimensionLabel.text = "Dimension: "
+        dimensionLabel.textColor = .orange
+        dimensionLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        dimensionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return dimensionLabel
     }()
     
     private lazy var featureLabel1: UILabel = {
@@ -186,75 +165,107 @@ class LocationDetailViewController: UIViewController {
         return featureLabel3
     }()
     
-    private lazy var featureLabel4: UILabel = {
-        let featureLabel3 = UILabel()
-        featureLabel3.text = "Earth"
-        featureLabel3.textColor = .black
-        featureLabel3.font = UIFont.systemFont(ofSize: 20.0)
-        featureLabel3.translatesAutoresizingMaskIntoConstraints = false
-        return featureLabel3
-    }()
-    
-    private lazy var featureLabel5: UILabel = {
-        let featureLabel3 = UILabel()
-        featureLabel3.text = "Earth"
-        featureLabel3.textColor = .black
-        featureLabel3.font = UIFont.systemFont(ofSize: 20.0)
-        featureLabel3.translatesAutoresizingMaskIntoConstraints = false
-        return featureLabel3
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(detailImageView)
-        view.addSubview(titleLabel)
-        view.addSubview(detailView)
+        view.addSubview(locationDetailImageView)
+        view.addSubview(titleAndDetailViewtackView)
         view.addSubview(stackView)
+        view.addSubview(charactersCollectionView)
+        view.addSubview(residentsLabel)
         
-        stackViewImageAndLabel.addArrangedSubview(statusLabel)
-        stackViewImageAndLabel.addArrangedSubview(statusImageView)
-        stackViewImageAndLabel.addArrangedSubview(featureLabel1)
+        titleStackView.addArrangedSubview(typeLabel)
+        titleStackView.addArrangedSubview(featureLabel1)
         
-        speciesStackView.addArrangedSubview(spesiecLabel)
-        speciesStackView.addArrangedSubview(featureLabel2)
+        firstEpisodeStackView.addArrangedSubview(firstEpisodeLabel)
+        firstEpisodeStackView.addArrangedSubview(featureLabel2)
         
-        genderStackView.addArrangedSubview(genderLabel)
-        genderStackView.addArrangedSubview(featureLabel3)
+        dimensionStackView.addArrangedSubview(dimensionLabel)
+        dimensionStackView.addArrangedSubview(featureLabel3)
         
-        origineStackView.addArrangedSubview(origineLabel)
-        origineStackView.addArrangedSubview(featureLabel4)
+        stackView.addArrangedSubview(titleStackView)
+        stackView.addArrangedSubview(firstEpisodeStackView)
+        stackView.addArrangedSubview(dimensionStackView)
         
-        locationStackView.addArrangedSubview(locationLabel)
-        locationStackView.addArrangedSubview(featureLabel5)
+        titleAndDetailViewtackView.addArrangedSubview(titleLabel)
+        titleAndDetailViewtackView.addArrangedSubview(detailView)
         
-        stackView.addArrangedSubview(stackViewImageAndLabel)
-        stackView.addArrangedSubview(speciesStackView)
-        stackView.addArrangedSubview(genderStackView)
-        stackView.addArrangedSubview(origineStackView)
-        stackView.addArrangedSubview(locationStackView)
+        charactersCollectionView.register(CharactersCollectionViewCell.self, forCellWithReuseIdentifier: CharactersCollectionViewCell.identifier)
+        charactersCollectionView.delegate = self
+        charactersCollectionView.dataSource = self
         
         NSLayoutConstraint.activate([
-            detailImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            detailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            detailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            detailImageView.heightAnchor.constraint(equalToConstant: 400),
+            locationDetailImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            locationDetailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            locationDetailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            locationDetailImageView.heightAnchor.constraint(equalToConstant: 400),
             
-            titleLabel.topAnchor.constraint(equalTo: detailImageView.bottomAnchor, constant: 16),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: locationDetailImageView.bottomAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
             
-            detailView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            detailView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5),
             detailView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             detailView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10),
-            detailView.heightAnchor.constraint(equalToConstant: 300),
+            detailView.heightAnchor.constraint(equalToConstant: 120),
             
-            stackView.topAnchor.constraint(equalTo: detailView.topAnchor, constant: 16),
+            stackView.topAnchor.constraint(equalTo: detailView.topAnchor, constant: 10),
             stackView.leadingAnchor.constraint(equalTo: detailView.leadingAnchor, constant: 20),
             stackView.trailingAnchor.constraint(equalTo: detailView.trailingAnchor, constant: -20),
-            stackView.bottomAnchor.constraint(equalTo: detailView.bottomAnchor, constant: -16),
+            
+            residentsLabel.topAnchor.constraint(equalTo: detailView.bottomAnchor, constant: 10),
+            residentsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            residentsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            
+            charactersCollectionView.topAnchor.constraint(equalTo: residentsLabel.bottomAnchor, constant: 10),
+            charactersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            charactersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            charactersCollectionView.heightAnchor.constraint(equalToConstant: 100),
         ])
     }
+}
+
+
+//MARK: Collection View Data Source
+extension LocationDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
     
-   
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharactersCollectionViewCell.identifier, for: indexPath) as? CharactersCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.nameLabel.isHidden = true
+        return cell
+    }
+}
+
+//MARK: Collection View Delegate
+extension LocationDetailViewController: UICollectionViewDelegate {
+}
+
+extension LocationDetailViewController: UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5.0, left: 15.0, bottom: 5.0, right: 15.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == charactersCollectionView {
+            return CGSize(width: 90, height: 90)
+            
+        } else {
+            return CGSize(width: view.frame.width, height: 120)
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
+    }
 }
